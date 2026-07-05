c552c00697eb1fa8b1c5f24c0493548bcc6b180f
lightkyo <shadow020205@gmail.com>
2026-07-05 20:37:06 +0900

    サイトURLをavoidanistblogに更新

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://lightkyo.github.io",
+	site: "https://lightkyo.github.io/avoidanistblog",
 	// site: "https://www.avoidanist.com",
 	base: "/",
 	trailingSlash: "always",
